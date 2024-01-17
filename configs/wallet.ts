3c7bcec597c1b31b16e6111eaa8cbdb6e0a44b5c
spennyp <hello@spencerperkins.me>
2024-01-16 17:29:40 -0700

    Added base and base-goerli chains

--- configs/wallet.ts
+++ configs/wallet.ts
@@ -3,47 +3,51 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { mainnet, goerli, configureChains, createClient } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { publicProvider } from "wagmi/providers/public";
-import { zoraTestnet, zora } from "@wagmi/chains";
+import { zoraTestnet, zora, base, baseGoerli } from "@wagmi/chains";
 
 const selectedChain = {
-  "1": mainnet,
-  "5": goerli,
-  "999": zoraTestnet,
-  "7777777": zora,
+    "1": mainnet,
+    "5": goerli,
+    "999": zoraTestnet,
+    "7777777": zora,
+    "8453": base,
+    "84531": baseGoerli,
 }[process.env.NEXT_PUBLIC_TOKEN_NETWORK ?? "1"]!;
 
 export const RPC_URL = {
-  "1": `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-  "5": `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-  "999": "https://testnet.rpc.zora.energy",
-  "7777777": "https://rpc.zora.energy",
+    "1": `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    "5": `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    "999": "https://testnet.rpc.zora.energy",
+    "7777777": "https://rpc.zora.energy",
+    "8453": `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    "84531": `https://base-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
 }[process.env.NEXT_PUBLIC_TOKEN_NETWORK ?? "1"]!;
 
-export type ChainId = "1" | "5" | "999" | "7777777";
+export type ChainId = "1" | "5" | "999" | "7777777" | "8453" | "84531";
 
 const { chains, provider } = configureChains(
-  [selectedChain],
-  [
-    jsonRpcProvider({
-      rpc: (_) => {
-        return { http: RPC_URL };
-      },
-      stallTimeout: 1000,
-    }),
-    publicProvider(),
-  ]
+    [selectedChain],
+    [
+        jsonRpcProvider({
+            rpc: (_) => {
+                return { http: RPC_URL };
+            },
+            stallTimeout: 1000,
+        }),
+        publicProvider(),
+    ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains,
-  projectId: "12fcc83f53d043bf4282e8233ef1aad7",
+    appName: "My RainbowKit App",
+    chains,
+    projectId: "12fcc83f53d043bf4282e8233ef1aad7",
 });
 
 const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
+    autoConnect: true,
+    connectors,
+    provider,
 });
 
 export { wagmiClient, chains };
--- constants/urls.ts
+++ constants/urls.ts
@@ -1,10 +1,13 @@
+import { base, baseGoerli } from "@wagmi/chains";
+
 export const ETHERSCAN_BASEURL = {
-  "1": "https://etherscan.io",
-  "5": "https://goerli.etherscan.io",
-  "999": "https://explorer.zora.energy/",
-  "7777777": "https://testnet.explorer.zora.energy/",
+    "1": "https://etherscan.io",
+    "5": "https://goerli.etherscan.io",
+    "999": "https://explorer.zora.energy/",
+    "7777777": "https://testnet.explorer.zora.energy/",
+    "8453": base.blockExplorers.etherscan.url,
+    "84531": baseGoerli.blockExplorers.etherscan.url,
 }[process.env.NEXT_PUBLIC_TOKEN_NETWORK ?? "1"];
 
 export const ETHER_ACTOR_BASEURL = "https://ether.actor";
-export const IPFS_GATEWAY =
-  process.env.NEXT_PUBLIC_IPFS_GATEWAY || "https://gateway.pinata.cloud/ipfs/";
+export const IPFS_GATEWAY = process.env.NEXT_PUBLIC_IPFS_GATEWAY || "https://gateway.pinata.cloud/ipfs/";
