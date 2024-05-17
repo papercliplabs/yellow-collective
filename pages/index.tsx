61ec8bb7a78ddb32d878a77d13431032556997e4
Spencer Perkins <31705429+spennyp@users.noreply.github.com>
2024-05-16 23:08:09 -0700

    Add frame (#5)
    * Added auction frame to yellowcollective.xyz
    
    * Added auction frame

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@ import { useIsMounted } from "hooks/useIsMounted";
 import { Fragment } from "react";
 import Hero from "../components/Hero/Hero";
 import { GetStaticPropsResult, InferGetStaticPropsType } from "next";
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { SWRConfig } from "swr";
 import {
   ContractInfo,
@@ -13,17 +12,13 @@ import {
   TokenInfo,
 } from "data/nouns-builder/token";
 import { AuctionInfo, getCurrentAuction } from "data/nouns-builder/auction";
-import { promises as fs } from "fs";
-import path from "path";
 import Footer from "@/components/Footer";
-import FaqElement from "@/components/FaqElement";
 import { getAddresses } from "@/services/nouns-builder/manager";
-import Image from "next/image";
 import Banner from "@/components/Banner";
 import Faq from "@/components/Faq";
 import Description from "@/components/Description";
-
-type MarkdownSource = MDXRemoteSerializeResult<Record<string, unknown>>;
+import Head from "next/head";
+import { getFrameMetadata } from "@/utils/getFrameMetadata";
 
 export const getStaticProps = async (): Promise<
   GetStaticPropsResult<{
@@ -32,64 +27,34 @@ export const getStaticProps = async (): Promise<
     contract: ContractInfo;
     token: TokenInfo;
     auction: AuctionInfo;
-    descriptionSource: MarkdownSource;
-    faqSources: MarkdownSource[];
+    frameTags: { property: string; content: string }[];
   }>
 > => {
   // Get token and auction info
   const tokenContract = process.env
     .NEXT_PUBLIC_TOKEN_CONTRACT! as `0x${string}`;
 
-  const addresses = await getAddresses({ tokenAddress: tokenContract });
-
-  const [contract, auction] = await Promise.all([
+  const [addresses, contract, frameMetadata] = await Promise.all([
+    getAddresses({ tokenAddress: tokenContract }),
     getContractInfo({ address: tokenContract }),
-    getCurrentAuction({ address: addresses.auction }),
+    getFrameMetadata(
+      `https://frames.paperclip.xyz/nounish-auction/yellow-collective`
+    ),
   ]);
 
+  const auction = await getCurrentAuction({ address: addresses.auction });
   const tokenId = auction.tokenId;
   const token = await getTokenInfo({
     address: tokenContract,
     tokenid: tokenId,
   });
 
-  // Get description and faq markdown
-
-  const templateDirectory = path.join(process.cwd(), "templates");
-  const descFile = await fs.readFile(
-    templateDirectory + "/home/description.md",
-    "utf8"
-  );
-  const descMD = await serialize(descFile);
-
-  let faqSources: MarkdownSource[] = [];
-  try {
-    const faqFiles = await fs.readdir(templateDirectory + "/home/faq", {
-      withFileTypes: true,
-    });
-
-    faqSources = await Promise.all(
-      faqFiles
-        .filter((dirent) => dirent.isFile())
-        .map(async (file) => {
-          const faqFile = await fs.readFile(
-            templateDirectory + "/home/faq/" + file.name,
-            "utf8"
-          );
-
-          return serialize(faqFile, { parseFrontmatter: true });
-        })
-    ).then((x) =>
-      x.sort(
-        (a, b) =>
-          Number(a.frontmatter?.order || 0) - Number(b.frontmatter?.order || 0)
-      )
-    );
-  } catch {
-    //Do Nothing (no FAQ directory)
-  }
-
   if (!contract.image) contract.image = "";
+
+  // Only take fc:frame tags (not og image overrides)
+  const filteredFrameMetadata = frameMetadata.filter((entry) =>
+    entry.property.includes("fc:frame")
+  );
 
   return {
     props: {
@@ -98,8 +63,7 @@ export const getStaticProps = async (): Promise<
       contract,
       token,
       auction,
-      descriptionSource: descMD,
-      faqSources,
+      frameTags: filteredFrameMetadata,
     },
     revalidate: 60,
   };
@@ -111,12 +75,9 @@ export default function SiteComponent({
   contract,
   token,
   auction,
-  descriptionSource,
-  faqSources,
+  frameTags,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const isMounted = useIsMounted();
-
-  if (!isMounted) return <Fragment />;
 
   return (
     <SWRConfig
@@ -128,14 +89,21 @@ export default function SiteComponent({
         },
       }}
     >
-      <div className="bg-accent min-h-screen flex flex-col items-center justify-start w-screen">
-        <Banner />
-        <Header />
-        <Hero />
-        <Description />
-        <Faq />
-        <Footer />
-      </div>
+      <Head>
+        {frameTags.map(({ property, content }, i) => (
+          <meta property={property} content={content} key={i} />
+        ))}
+      </Head>
+      {isMounted && (
+        <div className="bg-accent min-h-screen flex flex-col items-center justify-start w-screen">
+          <Banner />
+          <Header />
+          <Hero />
+          <Description />
+          <Faq />
+          <Footer />
+        </div>
+      )}
     </SWRConfig>
   );
 }
